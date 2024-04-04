4f128fcf04e8663b7f3e2db5b82a39d33577490a
Andrey Cheban <anceb@yandex.ru>
2024-04-04 12:17:27 +0300

    Create navbar

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,28 @@
 </script>
   
 <template>
+<div class="wrapper" data-bs-theme="dark">
 
+  <nav class="navbar bg-body-tertiary">
+    
+    <div class="container-fluid">
+      <span class="navbar-brand mb-0 h1">Create a new task!</span>
+
+      <div class="buttons-wrapper" role="group">
+        <button type="button" class="btn btn-light">All</button>
+        <button type="button" class="btn btn-primary">Active</button>
+        <button type="button" class="btn btn-success">Completed</button>
+      </div>
+
+      <button type="button" class="btn btn-warning" disabled>Clear completed</button>
+
+      <div class="input-group">
+        <span class="input-group-text">To do:</span>
+        <textarea class="form-control" aria-label="With textarea"></textarea>
+      </div>
+    </div>
+
+  </nav>
+
+</div>
 </template>
-
-<style scoped>
-
-</style>
